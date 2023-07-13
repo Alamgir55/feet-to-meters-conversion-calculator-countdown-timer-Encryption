5048849e85d227412a801516ee5cff3efb9d23b8
Alamgir55 <alamgir555hossain@gmail.com>
2023-07-12 23:38:30 -0400

    encrypt-decrypt

---
+++ encryption/encryption.py
@@ -0,0 +1,11 @@
+encrypt = input(" Enter text encrypt: \n")
+
+encrypted_text = ''
+for c in encrypt:
+    x = ord(c)
+    x = x + 1
+    c2 = chr(x)
+    encrypted_text = encrypted_text + c2
+print(encrypted_text)
+
+print("\n")
